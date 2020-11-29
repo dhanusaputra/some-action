04c870194e8a1da656c0f27f82d033b9536e7a0f
dhanu <andreasdhanu@gmail.com>
2020-11-30 00:11:22 +0700

    add write-good

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+// const writeGood = require('write-good');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput('in');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
