3642a0f9f36a082cdf00d5492a1b288b5a2c64fa
dhanu <andreasdhanu@gmail.com>
2020-11-30 09:07:48 +0700

    test

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const github = require('@actions/github');
 const writeGood = require('write-good');
 const fs = require('fs');
 
+const fileExt = ['.md', '.markdown', 'mkdn', 'mkd', 'mdown'];
+
 try {
   const files = core.getInput('files');
   console.log(`Hello ${files}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  fs.readFile(files, 'utf8', (err, data) => {
+  if (fileExt.includes(files.split('.').pop())) fs.readFile(files, 'utf8', (err, data) => {
     if (err) core.setFailed(err);
     const suggestions = writeGood(data);
     if (suggestions.length > 0) core.setFailed(suggestions);
