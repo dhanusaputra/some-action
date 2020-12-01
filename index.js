b857880aab9e5fdb212a75d8ece266c4c8fbf4b5
dhanu <andreasdhanu@gmail.com>
2020-12-01 08:07:12 +0700

    test

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ const fs = require('fs');
 const fileExt = ['.md', '.markdown', 'mkdn', 'mkd', 'mdown'];
 
 try {
-  const files = core.getInput('files');
+  const files = core.getInput('files').split(/\r?\n/);
   console.log(`Hello ${files}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  if (fileExt.includes(files.split('.').pop().toLowerCase())) fs.readFile(files, 'utf8', (err, data) => {
-    if (err) core.setFailed(err);
-    const suggestions = writeGood(data);
-    if (suggestions.length > 0) core.setFailed(suggestions);
-  });
+  files.forEach(file) => {
+    if (fileExt.includes(file.split('.').pop().toLowerCase())) fs.readFile(files, 'utf8', (err, data) => {
+      if (err) core.setFailed(err);
+      const suggestions = writeGood(data);
+      if (suggestions.length > 0) core.setFailed(suggestions);
+    });
+  }
 } catch (err) {
   core.setFailed(err.message);
 }
